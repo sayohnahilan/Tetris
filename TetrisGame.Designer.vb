e1bcf5ce6f6dde70d76a24e11da447a7cca1a309
Sayohn Ahilan <55256294+sayohnahilan@users.noreply.github.com>
2019-09-19 21:01:28 -0400

    Add files via upload

---
+++ TetrisGame.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TetrisGame
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.HelpLabel = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.LinesLabel = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.SpeedLabel = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.ScoreLabel = New System.Windows.Forms.Label
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.LevelLabel = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.GameBox = New System.Windows.Forms.PictureBox
+        Me.MessageLabel = New System.Windows.Forms.Label
+        Me.PreviewBox = New System.Windows.Forms.PictureBox
+        Me.StylesLabel = New System.Windows.Forms.Label
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        CType(Me.GameBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PreviewBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'HelpLabel
+        '
+        Me.HelpLabel.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.HelpLabel.Location = New System.Drawing.Point(220, 241)
+        Me.HelpLabel.Name = "HelpLabel"
+        Me.HelpLabel.Size = New System.Drawing.Size(200, 65)
+        Me.HelpLabel.TabIndex = 28
+        Me.HelpLabel.Text = "Left   Move Left|Right  Move Right|Down   Move Down|Up     Rotate Block|N      Ne" & _
+            "w Game|P      Pause Game|B      Classic Brick Style|M      Molten Metal Style|"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(220, 221)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 16)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "Keys:"
+        '
+        'LinesLabel
+        '
+        Me.LinesLabel.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinesLabel.ForeColor = System.Drawing.Color.DarkRed
+        Me.LinesLabel.Location = New System.Drawing.Point(342, 144)
+        Me.LinesLabel.Name = "LinesLabel"
+        Me.LinesLabel.Size = New System.Drawing.Size(54, 16)
+        Me.LinesLabel.TabIndex = 25
+        Me.LinesLabel.Text = "0"
+        Me.LinesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(342, 122)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(54, 16)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "Lines"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'SpeedLabel
+        '
+        Me.SpeedLabel.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SpeedLabel.ForeColor = System.Drawing.Color.DarkRed
+        Me.SpeedLabel.Location = New System.Drawing.Point(277, 144)
+        Me.SpeedLabel.Name = "SpeedLabel"
+        Me.SpeedLabel.Size = New System.Drawing.Size(54, 16)
+        Me.SpeedLabel.TabIndex = 23
+        Me.SpeedLabel.Text = "1"
+        Me.SpeedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(277, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 16)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "Speed"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ScoreLabel
+        '
+        Me.ScoreLabel.Font = New System.Drawing.Font("Courier New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ScoreLabel.ForeColor = System.Drawing.Color.Red
+        Me.ScoreLabel.Location = New System.Drawing.Point(305, 29)
+        Me.ScoreLabel.Name = "ScoreLabel"
+        Me.ScoreLabel.Size = New System.Drawing.Size(104, 26)
+        Me.ScoreLabel.TabIndex = 21
+        Me.ScoreLabel.Text = "000"
+        Me.ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(315, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 16)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Your Score"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'LevelLabel
+        '
+        Me.LevelLabel.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LevelLabel.ForeColor = System.Drawing.Color.DarkRed
+        Me.LevelLabel.Location = New System.Drawing.Point(220, 144)
+        Me.LevelLabel.Name = "LevelLabel"
+        Me.LevelLabel.Size = New System.Drawing.Size(54, 16)
+        Me.LevelLabel.TabIndex = 19
+        Me.LevelLabel.Text = "1"
+        Me.LevelLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(220, 122)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 16)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Level"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GameBox
+        '
+        Me.GameBox.BackColor = System.Drawing.Color.DimGray
+        Me.GameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.GameBox.Location = New System.Drawing.Point(12, 13)
+        Me.GameBox.Name = "GameBox"
+        Me.GameBox.Size = New System.Drawing.Size(200, 400)
+        Me.GameBox.TabIndex = 31
+        Me.GameBox.TabStop = False
+        '
+        'MessageLabel
+        '
+        Me.MessageLabel.BackColor = System.Drawing.Color.Black
+        Me.MessageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.MessageLabel.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MessageLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MessageLabel.ForeColor = System.Drawing.Color.Yellow
+        Me.MessageLabel.Location = New System.Drawing.Point(39, 100)
+        Me.MessageLabel.Name = "MessageLabel"
+        Me.MessageLabel.Size = New System.Drawing.Size(139, 197)
+        Me.MessageLabel.TabIndex = 32
+        Me.MessageLabel.Text = "WELCOME TO TETRIS"
+        Me.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PreviewBox
+        '
+        Me.PreviewBox.BackColor = System.Drawing.Color.Transparent
+        Me.PreviewBox.Location = New System.Drawing.Point(223, 39)
+        Me.PreviewBox.Name = "PreviewBox"
+        Me.PreviewBox.Size = New System.Drawing.Size(80, 80)
+        Me.PreviewBox.TabIndex = 33
+        Me.PreviewBox.TabStop = False
+        '
+        'StylesLabel
+        '
+        Me.StylesLabel.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StylesLabel.Location = New System.Drawing.Point(220, 341)
+        Me.StylesLabel.Name = "StylesLabel"
+        Me.StylesLabel.Size = New System.Drawing.Size(200, 68)
+        Me.StylesLabel.TabIndex = 35
+        Me.StylesLabel.Text = "B      Classic Bricks|M      Molten Metal|F      Fire|S      Sky|W      Water|C  " & _
+            "    Chocolate"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(220, 319)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(47, 16)
+        Me.Label7.TabIndex = 34
+        Me.Label7.Text = "Style:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(220, 13)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 16)
+        Me.Label6.TabIndex = 36
+        Me.Label6.Text = "Next"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'TetrisGame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 430)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.StylesLabel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PreviewBox)
+        Me.Controls.Add(Me.MessageLabel)
+        Me.Controls.Add(Me.GameBox)
+        Me.Controls.Add(Me.HelpLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LinesLabel)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.SpeedLabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ScoreLabel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LevelLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "TetrisGame"
+        Me.Text = "TetrisGame"
+        CType(Me.GameBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PreviewBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents HelpLabel As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents LinesLabel As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents SpeedLabel As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ScoreLabel As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents LevelLabel As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GameBox As System.Windows.Forms.PictureBox
+    Friend WithEvents MessageLabel As System.Windows.Forms.Label
+    Friend WithEvents PreviewBox As System.Windows.Forms.PictureBox
+    Friend WithEvents StylesLabel As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+End Class
---
+++ TetrisGame.vb
@@ -0,0 +1,221 @@
+﻿Option Strict On
+Imports MyGames.Tetris
+Imports MyGames.Tetris.TetrisBlock
+
+Partial Class TetrisGame
+    Private GameBoard As TetrisBoard
+    Private FallingBlock As TetrisBlock
+    Private PreviewBoard As TetrisBoard
+    Private PreviewBlock As TetrisBlock
+
+    Private Score As Double
+    Private Level As Integer
+    Private Speed As Integer
+    Private Lines As Integer
+
+    Private RandomNumbers As New Random
+    Private Status As GameStatus = GameStatus.Stopped
+
+    Private Enum GameStatus
+        Running
+        Paused
+        Stopped
+    End Enum
+
+#Region "Event Handlers"
+    Private Sub TetrisGame_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        PreviewBoard = New TetrisBoard(PreviewBox)
+        With PreviewBoard
+            .Rows = 4
+            .Columns = 4
+            .CellSize = New Size(20, 20)
+            .Style = BorderStyle.FixedSingle
+            .SetupBoard()
+        End With
+        PreviewBlock = New TetrisBlock(PreviewBoard)
+        PreviewBlock.CenterCell = PreviewBoard.Cells(2, 2)
+        PreviewBlock.Shape = GetRandomShape()
+
+        GameBoard = New TetrisBoard(GameBox)
+        With GameBoard
+            .Rows = 20
+            .Columns = 10
+            .CellSize = New Size(20, 20)
+            .Style = BorderStyle.FixedSingle
+            .SetupBoard()
+        End With
+        FallingBlock = New TetrisBlock(GameBoard)
+
+        HelpLabel.Text = HelpLabel.Text.Replace("|", vbCrLf)
+        StylesLabel.Text = StylesLabel.Text.Replace("|", vbCrLf)
+        ShowMessage(String.Format("{0}W E L C O M E{0}{0}T O{0}{0}T E T R I S{0}{0}{0}{0}Click here to start new game", vbCrLf))
+
+    End Sub
+
+    Private Sub TetrisGame_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Left, Keys.Right, Keys.Down, Keys.Up
+                If Status = GameStatus.Running Then
+                    With FallingBlock
+                        Select Case e.KeyCode
+                            Case Keys.Left
+                                If .CanMove(MoveDirection.Left) Then .Move(MoveDirection.Left)
+                            Case Keys.Right
+                                If .CanMove(MoveDirection.Right) Then .Move(MoveDirection.Right)
+                            Case Keys.Down
+                                If .CanMove(MoveDirection.Down) Then .Move(MoveDirection.Down)
+                            Case Keys.Up
+                                If .CanRotate Then .Rotate()
+                        End Select
+                    End With
+                End If
+            Case Keys.P
+                If Status <> GameStatus.Stopped Then TogglePauseGame()
+            Case Keys.N
+                If Status = GameStatus.Stopped Then
+                    StartNewGame()
+                ElseIf DialogResult.Yes = MessageBox.Show("Abort current game?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) Then
+                    StartNewGame()
+                End If
+            Case Keys.B
+                GameBoard.Style = BorderStyle.FixedSingle
+                PreviewBoard.Style = BorderStyle.FixedSingle
+                GameBoard.Color = Color.Empty
+                PreviewBoard.Color = Color.Empty
+            Case Keys.M
+                GameBoard.Style = BorderStyle.None
+                PreviewBoard.Style = BorderStyle.None
+                GameBoard.Color = Color.Empty
+                PreviewBoard.Color = Color.Empty
+            Case Keys.F
+                GameBoard.Style = BorderStyle.None
+                PreviewBoard.Style = BorderStyle.None
+                GameBoard.Color = Color.Orange
+                PreviewBoard.Color = Color.Orange
+            Case Keys.S
+                GameBoard.Style = BorderStyle.None
+                PreviewBoard.Style = BorderStyle.None
+                GameBoard.Color = Color.SkyBlue
+                PreviewBoard.Color = Color.SkyBlue
+            Case Keys.W
+                GameBoard.Style = BorderStyle.None
+                PreviewBoard.Style = BorderStyle.None
+                GameBoard.Color = Color.Blue
+                PreviewBoard.Color = Color.Blue
+            Case Keys.C
+                GameBoard.Style = BorderStyle.None
+                PreviewBoard.Style = BorderStyle.None
+                GameBoard.Color = Color.Chocolate
+                PreviewBoard.Color = Color.Chocolate
+        End Select
+    End Sub
+
+    Private Sub MessageLabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MessageLabel.Click
+        Select Case Status
+            Case GameStatus.Stopped
+                StartNewGame()
+            Case GameStatus.Paused
+                TogglePauseGame()
+        End Select
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If FallingBlock.CanMove(MoveDirection.Down) Then
+            FallingBlock.Move(MoveDirection.Down)
+        Else
+            '' Fix block to base
+            For Each cell As TetrisCell In FallingBlock.Cells
+                cell.IsEmpty = False
+            Next
+
+            '' Remove completed rows
+            Dim checkRows = From cell In FallingBlock.Cells _
+                            Order By cell.Row _
+                            Select cell.Row Distinct
+            Dim rowsRemoved As Integer = 0
+            For Each row In checkRows
+                If GameBoard.IsRowComplete(row) Then
+                    GameBoard.RemoveRow(row)
+                    rowsRemoved += 1
+                End If
+            Next
+
+            '' Update Statistics
+            Score += Math.Pow(rowsRemoved, 2) * 100
+            Lines += rowsRemoved
+            Speed = 1 + Lines \ 10
+            If Speed Mod 10 = 0 Then Level += 1 : Speed = 1
+            Timer1.Interval = (10 - Speed) * 100
+            UpdateStatistics()
+
+            '' Make the next block fall
+            DropNextFallingBlock()
+
+            '' Check if game has ended
+            If Not FallingBlock.CanMove(FallingBlock.CenterCell) Then EndGame()
+        End If
+    End Sub
+#End Region
+
+#Region "Private Methods"
+    Private Function GetRandomShape() As TetrisBlock.Shapes
+        Dim number As Integer = RandomNumbers.Next(Shapes.I1, Shapes.Z4 + 1) 'first to last
+        Return CType(number, TetrisBlock.Shapes)
+    End Function
+
+    Private Sub DropNextFallingBlock()
+        FallingBlock.CenterCell = GameBoard.Cells(2, GameBoard.Columns \ 2)
+        FallingBlock.Shape = PreviewBlock.Shape
+        PreviewBlock.Shape = GetRandomShape()
+        PreviewBlock.RefreshBackGround()
+        PreviewBlock.Refresh()
+    End Sub
+
+    Private Sub StartNewGame()
+        Score = 0
+        Lines = 0
+        Speed = 1
+        Level = 1
+        Timer1.Interval = 1000
+        UpdateStatistics()
+
+        GameBoard.SetupBoard()
+        'PreviewBlock.Shape = GetRandomShape()
+        DropNextFallingBlock()
+        MessageLabel.Visible = False
+        Timer1.Enabled = True
+        Status = GameStatus.Running
+    End Sub
+
+    Private Sub EndGame()
+        Timer1.Enabled = False
+        Status = GameStatus.Stopped
+        ShowMessage(String.Format("{0}{0}GAME OVER{0}{0}{0}{0}Click here to start new game", vbCrLf))
+    End Sub
+
+    Private Sub UpdateStatistics()
+        ScoreLabel.Text = Score.ToString("000")
+        LinesLabel.Text = Lines.ToString
+        LevelLabel.Text = Level.ToString
+        SpeedLabel.Text = Speed.ToString
+    End Sub
+
+    Private Sub TogglePauseGame()
+        If Status = GameStatus.Paused Then
+            Status = GameStatus.Running
+            MessageLabel.Visible = False
+            Timer1.Enabled = True
+        Else
+            Status = GameStatus.Paused
+            ShowMessage(String.Format("{0}{0}GAME PAUSED{0}{0}{0}{0}Click here to resume.", vbCrLf))
+        End If
+    End Sub
+
+    Private Sub ShowMessage(ByVal message As String)
+        MessageLabel.Text = message
+        MessageLabel.Visible = True
+        Timer1.Enabled = False
+    End Sub
+#End Region
+
+End Class
